f582f423f7303922220a9e11e6a7e97affd292a1
KirillSpitsyn <zima197211@gmail.com>
2024-08-18 07:22:12 -0400

    Update Orchestration.sol

--- Orchestration.sol
+++ Orchestration.sol
@@ -25,6 +25,17 @@ contract Orchestrator is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 demurrageRate = tcoin.getDemurrageRate();
     uint256 reserveRatio = calculateReserveRatio();
 
+    // Enum for voting options
+    enum VoteOption { Leave, Increment, Decrement }
+    
+    // Struct for holding votes
+    struct Vote {
+        uint256 incrementVotes;
+        uint256 decrementVotes;
+        uint256 leaveVotes;
+        uint256 totalVotes;
+    }
+
     // Mappings for charity names and addresses
     mapping(uint256 => string) public charityNames;
     mapping(uint256 => address) public charityAddresses;
@@ -34,6 +45,8 @@ contract Orchestrator is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     mapping(address => bool) public hasVoted; // Tracks whether a steward has voted
     mapping(address => uint256) public stewardVotes; // Tracks the current vote of each steward
     uint256[] public proposedPegValues; // List of proposed peg values
+    mapping(string => Vote) public votes; // Track votes for parameters
+    mapping(address => mapping(string => VoteOption)) public stewardVotesAll; // Track steward votes for remaining values
 
     function setTcoinAddress(address _tcoinAddress) external onlyOwner {
         tcoin = TCOIN(_tcoinAddress);
@@ -138,6 +151,157 @@ contract Orchestrator is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         // Update the peg value if there is a clear leader
         if (leadingPegValue != pegValue) {
             pegValue = leadingPegValue;
+        }
+    }
+
+    // Function to vote for multiple parameters
+    function voteToUpdateValues(
+        VoteOption voteOptionRedemptionRateUserTTC,
+        VoteOption voteOptionRedemptionRateStoreTTC,
+        VoteOption voteOptionRedemptionRateUserCAD,
+        VoteOption voteOptionRedemptionRateStoreCAD,
+        VoteOption voteOptionMinimumReserveRatio,
+        VoteOption voteOptionMaximumReserveRatio,
+        VoteOption voteOptionDemurrageRate,
+        VoteOption voteOptionReserveRatio
+    ) external {
+        address steward = msg.sender;
+        require(isSteward(steward), "Only stewards can vote");
+
+        // Check if the steward has voted before and decrement the vote count for the previous value
+        if (hasVoted[steward]) {
+            resetVotesForSteward(steward);
+        } else {
+            hasVoted[steward] = true;
+        }
+
+        // Update steward's votes
+        stewardVotesAll[steward]["redemptionRateUserTTC"] = voteOptionRedemptionRateUserTTC;
+        stewardVotesAll[steward]["redemptionRateStoreTTC"] = voteOptionRedemptionRateStoreTTC;
+        stewardVotesAll[steward]["redemptionRateUserCAD"] = voteOptionRedemptionRateUserCAD;
+        stewardVotesAll[steward]["redemptionRateStoreCAD"] = voteOptionRedemptionRateStoreCAD;
+        stewardVotesAll[steward]["minimumReserveRatio"] = voteOptionMinimumReserveRatio;
+        stewardVotesAll[steward]["maximumReserveRatio"] = voteOptionMaximumReserveRatio;
+        stewardVotesAll[steward]["demurrageRate"] = voteOptionDemurrageRate;
+        stewardVotesAll[steward]["reserveRatio"] = voteOptionReserveRatio;
+
+        // Process votes
+        processVote("redemptionRateUserTTC", voteOptionRedemptionRateUserTTC);
+        processVote("redemptionRateStoreTTC", voteOptionRedemptionRateStoreTTC);
+        processVote("redemptionRateUserCAD", voteOptionRedemptionRateUserCAD);
+        processVote("redemptionRateStoreCAD", voteOptionRedemptionRateStoreCAD);
+        processVote("minimumReserveRatio", voteOptionMinimumReserveRatio);
+        processVote("maximumReserveRatio", voteOptionMaximumReserveRatio);
+        processVote("demurrageRate", voteOptionDemurrageRate);
+        processVote("reserveRatio", voteOptionReserveRatio);
+
+        // Check and update values if necessary
+        checkAndUpdateValues("redemptionRateUserTTC");
+        checkAndUpdateValues("redemptionRateStoreTTC");
+        checkAndUpdateValues("redemptionRateUserCAD");
+        checkAndUpdateValues("redemptionRateStoreCAD");
+        checkAndUpdateValues("minimumReserveRatio");
+        checkAndUpdateValues("maximumReserveRatio");
+        checkAndUpdateValues("demurrageRate");
+        checkAndUpdateValues("reserveRatio");
+    }
+
+    function processVote(string memory valueName, VoteOption voteOption) internal {
+        if (voteOption == VoteOption.Increment) {
+            votes[valueName].incrementVotes++;
+        } else if (voteOption == VoteOption.Decrement) {
+            votes[valueName].decrementVotes++;
+        } else if (voteOption == VoteOption.Leave) {
+            votes[valueName].leaveVotes++;
+        }
+        votes[valueName].totalVotes++;
+    }
+
+    function checkAndUpdateValues(string memory valueName) internal {
+        if (votes[valueName].totalVotes * 2 >= stewardCount) { // 50% or more votes
+                VoteOption winningOption = getWinningOption(valueName);
+                updateValue(valueName, winningOption);
+            resetVotes(valueName);
+        }
+    }
+
+    function updateValue(string memory valueName, VoteOption winningOption) internal {
+        if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("redemptionRateUserTTC"))) {
+            if (winningOption == VoteOption.Increment) {
+                redemptionRateUserTTC = redemptionRateUserTTC * 101 / 100; // Increment by 1%
+            } else if (winningOption == VoteOption.Decrement) {
+                redemptionRateUserTTC = redemptionRateUserTTC * 99 / 100; // Decrement by 1%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("redemptionRateStoreTTC"))) {
+            if (winningOption == VoteOption.Increment) {
+                redemptionRateStoreTTC = redemptionRateStoreTTC * 101 / 100; // Increment by 1%
+            } else if (winningOption == VoteOption.Decrement) {
+                redemptionRateStoreTTC = redemptionRateStoreTTC * 99 / 100; // Decrement by 1%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("redemptionRateUserCAD"))) {
+            if (winningOption == VoteOption.Increment) {
+                redemptionRateUserCAD = redemptionRateUserCAD * 101 / 100; // Increment by 1%
+            } else if (winningOption == VoteOption.Decrement) {
+                redemptionRateUserCAD = redemptionRateUserCAD * 99 / 100; // Decrement by 1%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("redemptionRateStoreCAD"))) {
+            if (winningOption == VoteOption.Increment) {
+                redemptionRateStoreCAD = redemptionRateStoreCAD * 101 / 100; // Increment by 1%
+            } else if (winningOption == VoteOption.Decrement) {
+                redemptionRateStoreCAD = redemptionRateStoreCAD * 99 / 100; // Decrement by 1%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("minimumReserveRatio"))) {
+            if (winningOption == VoteOption.Increment) {
+                minimumReserveRatio = minimumReserveRatio + 10000; // Increment by 0.01%
+            } else if (winningOption == VoteOption.Decrement) {
+                minimumReserveRatio = minimumReserveRatio - 10000; // Decrement by 0.01%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("maximumReserveRatio"))) {
+            if (winningOption == VoteOption.Increment) {
+                maximumReserveRatio = maximumReserveRatio + 10000; // Increment by 0.01%
+            } else if (winningOption == VoteOption.Decrement) {
+                maximumReserveRatio = maximumReserveRatio - 10000; // Decrement by 0.01%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("demurrageRate"))) {
+            if (winningOption == VoteOption.Increment) {
+                demurrageRate = demurrageRate + 1; // Increment by 1%
+            } else if (winningOption == VoteOption.Decrement) {
+                demurrageRate = demurrageRate - 1; // Decrement by 1%
+            }
+        } else if (keccak256(abi.encodePacked(valueName)) == keccak256(abi.encodePacked("reserveRatio"))) {
+            if (winningOption == VoteOption.Increment) {
+                reserveRatio = reserveRatio + 10000; // Increment by 0.01%
+            } else if (winningOption == VoteOption.Decrement) {
+                reserveRatio = reserveRatio - 10000; // Decrement by 0.01%
+            }
+        }
+    }
+
+    function resetVotes(string memory valueName) internal {
+        votes[valueName].incrementVotes = 0;
+        votes[valueName].decrementVotes = 0;
+        votes[valueName].leaveVotes = 0;
+        votes[valueName].totalVotes = 0;
+    }
+
+    function resetVotesForSteward(address steward) internal {
+        stewardVotesAll[steward]["redemptionRateUserTTC"] = VoteOption.Leave;
+        stewardVotesAll[steward]["redemptionRateStoreTTC"] = VoteOption.Leave;
+        stewardVotesAll[steward]["redemptionRateUserCAD"] = VoteOption.Leave;
+        stewardVotesAll[steward]["redemptionRateStoreCAD"] = VoteOption.Leave;
+        stewardVotesAll[steward]["minimumReserveRatio"] = VoteOption.Leave;
+        stewardVotesAll[steward]["maximumReserveRatio"] = VoteOption.Leave;
+        stewardVotesAll[steward]["demurrageRate"] = VoteOption.Leave;
+        stewardVotesAll[steward]["reserveRatio"] = VoteOption.Leave;
+    }
+
+    function getWinningOption(string memory valueName) internal view returns (VoteOption) {
+        if (votes[valueName].incrementVotes > votes[valueName].decrementVotes && votes[valueName].incrementVotes > votes[valueName].leaveVotes) {
+            return VoteOption.Increment;
+        } else if (votes[valueName].decrementVotes > votes[valueName].incrementVotes && votes[valueName].decrementVotes > votes[valueName].leaveVotes) {
+            return VoteOption.Decrement;
+        } else {
+            return VoteOption.Leave;
         }
     }
 
