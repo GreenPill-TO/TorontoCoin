afd5226e998af53420106726289ee83500c1f4ea
KazanderDad <98373366+KazanderDad@users.noreply.github.com>
2025-01-31 01:18:02 -0500

    Update CADCOIN.sol
    Implemented the same changes here as recently implemented in the TTC Token contract.

--- CADCOIN.sol
+++ CADCOIN.sol
@@ -2,49 +2,47 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CAD is ERC20, Ownable (msg.sender) {
-    uint256 public totalMinted;
-    uint256 public totalBurned;
-    uint256 public totalTTCSupply;
+contract CAD is ERC20, AccessControl, Pausable {
+    using Counters for Counters.Counter;
+    
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("CADToken", "CAD") {}
+    Counters.Counter private _totalBurned; // Tracks total burned tokens
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    event Minted(address indexed to, uint256 amount);
+    event Burned(address indexed from, uint256 amount);
+
+    // Initialize the contract
+    constructor() ERC20("CAD Token", "CAD") {
+        _setupRole(OWNER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
+
+    // Mint new tokens
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) whenNotPaused {
         _mint(to, amount);
-        totalMinted += amount;
-
-        // Update Total Supply
-        if (totalMinted > 0) {
-            totalTTCSupply = totalMinted - totalBurned; 
-        }
+        emit Minted(to, amount);
     }
 
-    function burn(address from, uint256 amount) external onlyOwner {
-        _burn(from, amount);
-        totalBurned += amount;
-
-        // Update Total Supply
-        if (totalBurned > 0) {
-            totalTTCSupply = totalMinted - totalBurned; 
-        }
+    // Burn tokens from sender's account
+    function burn(uint256 amount) external whenNotPaused {
+        _burn(msg.sender, amount);
+        _totalBurned.increment();
+        emit Burned(msg.sender, amount);
     }
 
+    // View total minted tokens (calculated dynamically)
     function getTotalMinted() external view returns (uint256) {
-        return totalMinted;
+        return totalSupply() + _totalBurned.current();
     }
 
+    // View total burned tokens
     function getTotalBurned() external view returns (uint256) {
-        return totalBurned;
-    }
-
-    function getTotalCADCOINSupply() external view returns (uint256) {
-        if (totalTTCSupply != 0) {
-            return totalTTCSupply;
-        }
-        else{
-            return 0;
-        }
+        return _totalBurned.current();
     }
 }
